2145ce001b77924ac23db6daf52870a789ad2422
feurle <feurle@gmail.com>
2021-03-24 20:32:29 +0100

    nachricht

--- src/com/company/u3/Uebung3.java
+++ src/com/company/u3/Uebung3.java
@@ -3,6 +3,29 @@ package com.company.u3;
 public class Uebung3 {
 
     public static void main(String[] args) {
-        System.out.println("Uebung3");
+        int i = 0;
+        int j = 99;
+        int result = plus();
+        System.out.println(result);
     }
+
+    public static int plus(int i, int j) {
+        return i+j;
+    }
+
+    public static int plus() {
+        int i = 0;
+        int j = 99;
+        return i+j;
+    }
+
+    public static String plus(String input) {
+        return input;
+    }
+
+    public static void plus(int i) {
+        System.out.println(i);
+    }
+
+
 }
