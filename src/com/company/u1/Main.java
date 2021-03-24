c7f65a9036127dcbad62ad7c4182554b5c8a36d5
feurle <feurle@gmail.com>
2021-03-24 18:47:30 +0100

    refactored

--- src/com/company/u1/Main.java
+++ src/com/company/u1/Main.java
@@ -1,6 +1,7 @@
-package com.company;
+package com.company.u1;
 
 public class Main {
+
 
     public static void main(String[] args) {
 
---
+++ src/com/company/u3/Uebung3.java
@@ -0,0 +1,8 @@
+package com.company.u3;
+
+public class Uebung3 {
+
+    public static void main(String[] args) {
+        System.out.println("Uebung3");
+    }
+}
