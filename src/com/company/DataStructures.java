bba511f083bd822cfe22bd1928aabaa0dfe90a3c
feurle <feurle@gmail.com>
2021-03-24 18:15:14 +0100

    first commit

---
+++ src/com/company/DataStructures.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class DataStructures {
+
+    public static void main(String[] args) {
+
+        List<String> myList = new ArrayList<>();
+
+        Set<String> mySet = new HashSet<>();
+
+
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,44 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double weightOfApples = 3.5;
+        double pricePerAppleKilo = 1.5;
+
+        double weightOfBananas = 4.5;
+        double pricePerBananaKilo = 1.75;
+
+        System.out.println("my apple price:" + calculateTotalPrice(weightOfApples,pricePerAppleKilo));
+        System.out.println("my banana price:" + calculateTotalPrice(weightOfBananas,pricePerBananaKilo));
+
+
+        System.out.println("my apple price: " + calculateTotalPrice("APPLE", weightOfApples));
+        System.out.println("my banana price:" + calculateTotalPrice("BANANA", weightOfBananas));
+    }
+
+
+    public static double calculateTotalPrice(String fruit, double weight) {
+        double pricePerKilo = 0.0;
+
+        switch (fruit) {
+            case "APPLE":
+                pricePerKilo = 1.50;
+                break;
+            case "BANANA":
+                pricePerKilo = 1.75;
+                break;
+            default:
+                System.out.println("dont know the price");
+                break;
+        }
+
+        return calculateTotalPrice(weight, pricePerKilo);
+    }
+
+    public static double calculateTotalPrice(double weight, double pricePerKilo) {
+        double result = weight * pricePerKilo;
+        return result;
+    }
+}
